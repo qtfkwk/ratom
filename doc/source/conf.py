qtfkwk/ratom

commit 83c17c25e1a78a1c00d0b28438ddf8af63b0ffa9

fix version regex to match more than single final digit

setup.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     if 'update' in a:
         import datetime
         today = datetime.datetime.now().strftime('%Y-%m-%d')
-        vre = r'[0-9]*\.[0-9]*\.[0-9]'
+        vre = r'[0-9]*\.[0-9]*\.[0-9]+'
         files = 'ratom/*.py setup.py *.sh'
         doc = 'doc/source/index.rst'
         conf = 'doc/source/conf.py'

commit 38525cd3d36d47ce30ab4543c7768972610862d2

fix bug introduced by 83c17c25e1a78a1c00d0b28438ddf8af63b0ffa9 requiring sed -E

doc/source/conf.py
@@ -56,9 +56,9 @@ author = u'qtfkwk'
 # built documents.
 #
 # The short X.Y version.
-version = u'2.0.9'
+version = u'2.0.10'
 # The full version, including alpha/beta/rc tags.
-release = u'2.0.9'
+release = u'2.0.10'
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.
@@ -122,7 +122,7 @@ html_theme = 'alabaster'
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
-#html_title = u'ratom v2.0.9'
+#html_title = u'ratom v2.0.10'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None

ratom/common.py
@@ -9,7 +9,7 @@
 
 # Variables
 
-__version__ = '2.0.9'
+__version__ = '2.0.10'
 directory = '~/.ratom'
 conf = directory + '/config.json'
 defaults = dict(

setup.py
@@ -56,12 +56,12 @@ if __name__ == '__main__':
         doc = 'doc/source/index.rst'
         conf = 'doc/source/conf.py'
         c = [
-            r"sed -i _ 's/^__version__ = '\''%s'\''/" % vre + \
+            r"sed -Ei _ 's/^__version__ = '\''%s'\''/" % vre + \
                 r"__version__ = '\''%s'\''/' ratom/common.py" % v,
             r"sed -i _ 's/^# Date: .*$/# Date: %s/' %s" % (today, files),
             r"sed -i _ 's/^# Version: .*$/# Version: %s/' %s" % (v, files),
             r"sed -i _ 's/ratom-[0-9]*\.[0-9]*\.[0-9]*/ratom-%s/' %s" % (v, doc),
-            r"sed -i _ 's/%s/%s/' %s" % (vre, v, conf),
+            r"sed -Ei _ 's/%s/%s/' %s" % (vre, v, conf),
             r"rm -f ratom/*.py_ setup.py_ *.sh_ %s_ %s_" % (doc, conf),
         ]
         for i in c: