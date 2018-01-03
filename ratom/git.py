1200e7c9a42ed30902b114d739646ee07c09a4aa
Nick Jensen <nick@alienonesecurity.com>
2018-01-02 21:37:33 -0500

    release 2.1.5

--- ratom/git.py
+++ ratom/git.py
@@ -3,8 +3,8 @@
 """update Git repositories"""
 
 # File: ratom/git.py
-# Version: 2.1.4
-# Date: 2017-12-30
+# Version: 2.1.5
+# Date: 2018-01-02
 # Author: qtfkwk <qtfkwk+ratom@gmail.com>
 # Copyright: (C) 2016 by qtfkwk
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
